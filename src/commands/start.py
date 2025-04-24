b5afdc04e0941decd17a34c2a6c960735422310e
iBz-04 <issakaibrahimrayamah@gmail.com>
2025-04-24 16:01:35 +0300

    add: english translation

--- src/commands/start.py
+++ src/commands/start.py
@@ -3,10 +3,18 @@ from telegram.ext import CallbackContext
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(
+        "🇹🇷 Türkçe:\n\n"
         "🚨 Acil durum! Kullanılabilir komutlar:\n"
-        "• /bul <mahalle adı> veya <enlem> <boylam> ile en yakın toplanma alanını bulun\n"
+        "• /bul mahalle adı veya enlem, boylam ile en yakın toplanma alanını bulun\n"
         "• /acil ile İstanbul acil durum numaralarını görüntüleyin\n"
         "• /onlem ile deprem önlemlerini alın\n\n"
         "🏙️ Şu an sadece Tuzla Belediyesi sınırlarında çalışır.\n"
-        "Lütfen hızlı olun! ⚠️"
+        "Lütfen hızlı olun! ⚠️\n\n"
+        "🇬🇧 English:\n\n"
+        "🚨 Emergency commands available:\n"
+        "• /bul find nearest gathering spot by neighborhood name or latitude, longitude\n"
+        "• /acil show Istanbul emergency numbers\n"
+        "• /onlem show earthquake safety tips\n\n"
+        "🏙️ Works only within Tuzla Municipality.\n"
+        "Stay safe and act fast! ⚠️"
     )
